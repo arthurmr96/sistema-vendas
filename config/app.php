6d57673dedd87c4f50656896e42b0cbea75cd2d0
Arthur Ribeiro <arthurmr13@hotmail.com>
2019-10-12 13:48:29 -0300

    add laravel permission

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         Yajra\DataTables\HtmlServiceProvider::class,
         Yajra\DataTables\ButtonsServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
